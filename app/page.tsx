3a9e07394c36adeafc395564a91766557abb0947
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-07-30 10:45:47 +0200

    Add and configure shadcn add assets and install some dependancies.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            width={1000}
+            height={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+        </div>
+      </section>
     </div>
   );
 }
