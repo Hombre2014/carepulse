260a30d783e67c9c7dcc8cd37a3622b99b8bdfe9
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-08-02 21:05:25 +0200

    Building RegisterForm.

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,5 +1,47 @@
-const Registration = () => {
-  return <div>Registration</div>;
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { getUser } from '@/lib/actions/patient.actions';
+import RegisterForm from '@/components/forms/RegisterForm';
+
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification | PasskeyModal */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            width={1000}
+            height={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <RegisterForm user={user} />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/register-img.png"
+        width={1000}
+        height={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
+    </div>
+  );
 };
 
-export default Registration;
+export default Register;
